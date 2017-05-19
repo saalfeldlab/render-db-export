89833f1d005427c9a4c06cee99b39c3fb36c393e
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-05-19 13:27:28 -0400

    added fib26 example script

---
+++ run-render-montage-fib26.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OWN_DIR=`dirname "${BASH_SOURCE[0]}"`
+ABS_DIR=`readlink -f "$OWN_DIR"`
+
+FLINTSTONE=$ABS_DIR/flintstone/flintstone.sh
+JAR=$PWD/target/render-db-export-0.0.1-SNAPSHOT.jar # this jar must be accessible from the cluster
+CLASS=org.janelia.saalfeldlab.renderalign.RenderMontages
+N_NODES=20
+
+SERVER="http://tem-services.int.janelia.org:8080/render-ws/v1"
+OWNER="hessh"
+PROJECT="fly_em_pre_iso"
+STACK="col_26_temp_translation_1_98401"
+#STACK="n_10_l_9_tfac_m5_xlfac_0_ylfac_0_xfac_2_yfac_5_deg_2"
+#STACK="TOS_1_20948_fine_nbrs_10_lambda_6p5_deg_1"
+X="0"
+Y="0"
+WIDTH="37922"
+HEIGHT="7728"
+#WIDTH="7069"
+#HEIGHT="5083"
+#WIDTH="239959.0"
+#HEIGHT="148704.0"
+SCALE="0.05"
+OUTPUT="/nrs/saalfeld/tmp/spark-export/test/"
+ZSCALE="4"
+
+ARGV="-S $SERVER -u $OWNER -p $PROJECT -s $STACK -x $X -y $Y -w $WIDTH -h $HEIGHT -t $SCALE -o $OUTPUT -z $ZSCALE"
+#ARGV="-S $SERVER -u $OWNER -p $PROJECT -s $STACK -t $SCALE -o $OUTPUT"
+
+mkdir -p $OUTPUT
+
+SPARK_VERSION=rc TERMINATE=1 $FLINTSTONE $N_NODES $JAR $CLASS $ARGV
